1118cbeaeb6e8e517486db8abbaff87c2231007b
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-05-19 21:17:33 -0400

    update

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # os-contours-gpkg
 process contour data in gpkg from ordnance survey using @ngageoint/geopackage
 
+# usage
+```
+$ curl -C - -v -O -# https://s3-eu-west-1.amazonaws.com/os-open-data/os-terrain-50/geopackage/OSTerrain50_Contours.gpk
+$ node index.js > contours.ndjson
+$ tippecanoe ...
+```
 # source data
 As in [OS Terrain 50](http://data-format-trial-osonline.opendata.arcgis.com/pages/os-terrain-50), you can download the countour data of Great Britain like:
 ```
---
+++ index.js
@@ -0,0 +1,83 @@
+const config = require('config')
+const gpkg = require('@ngageoint/geopackage')
+const proj4 = require('proj4')
+
+proj4.defs("EPSG:27700","+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs");
+const local = proj4('EPSG:27700')
+
+const inverse = (geometry) => {
+  switch(geometry.type) {
+    case 'Point':
+      geometry.coordinates = local.inverse(geometry.coordinates)
+      break
+    case 'LineString':
+      for (let i in geometry.coordinates) {
+        geometry.coordinates[i] = local.inverse(geometry.coordinates[i])
+      }
+      break
+    default:
+      throw `${geometry.type} not supported.`
+  }
+  return geometry
+}
+
+const put = (fr) => {
+  if (fr.getGeometry().srsId !== 27700) throw `srid ${fr.getGeometry().srsId} unknown.`
+  let geometry = fr.getGeometry().geometry.toGeoJSON()
+  geometry = inverse(geometry)
+  // let properties = fr.values
+  let properties = {}
+  for (const k of ['prop_value', 'feat_type', 'sub_type']) {
+    if (fr.values[k]) properties[k] = fr.values[k]
+  }
+  delete properties[fr.getGeometryColumn().name]
+  let minzoom = 12
+  if (properties.feat_type === 'ContourLine') {
+    if (properties.prop_value % 400 === 0) {
+      minzoom = 10
+    } else if (properties.prop_value % 100 === 0) {
+      minzoom = 12
+    } else {
+      minzoom = 14
+    }
+  } else if (properties.feat_type === 'SpotHeight') {
+    minzoom = 14
+  } else if (properties.feat_type === 'LandWaterBoundary') {
+    if (properties.sub_type === 'meanLowWater') {
+      minzoom = 14
+    } else {
+      minzoom = 4
+    }
+  }
+  let geojson = {
+    type: 'Feature',
+    geometry: geometry,
+    properties: properties,
+    tippecanoe: {
+      layer: geometry.type.toLowerCase(), 
+      minzoom: minzoom, maxzoom: 14
+    }
+  }
+  console.log(JSON.stringify(geojson))
+}
+
+const dump = (path) => {
+  gpkg.GeoPackageManager.open(path, (err, gpkg) => {
+    if (err) throw err
+    gpkg.getFeatureTables((err, names) => {
+      if (err) throw err
+      for (const name of names) {
+        gpkg.getFeatureDaoWithTableName(name, (err, dao) => {
+          dao.queryForEach((err, row, rowDone) => {
+            put(dao.getFeatureRow(row))
+            rowDone()
+          })
+        })
+      }
+    })
+  })
+}
+
+for (let gpkg of config.get('gpkgs')) {
+  dump(gpkg)
+}
